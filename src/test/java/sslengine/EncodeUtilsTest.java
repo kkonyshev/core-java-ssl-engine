4b8dc3c64898e7e9a8c567fdf30013d151fc88b6
Konstantin <hs@ngs.ru>
2016-01-26 09:02:58 +0600

    EncodeUtils and tests

---
+++ src/test/java/sslengine/EncodeUtilsTest.java
@@ -0,0 +1,45 @@
+package sslengine;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.UUID;
+
+@RunWith(value = Parameterized.class)
+public class EncodeUtilsTest {
+
+    private Object object;
+
+    public EncodeUtilsTest(Object object){
+        this.object = object;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][] {
+                {
+                        UUID.randomUUID().toString()
+                },
+                {
+                        "short string here"
+                },
+                {
+                        Long.MAX_VALUE
+                },
+                {
+                        new ArrayList<>()
+                }
+            };
+        return Arrays.asList(data);
+    }
+
+    @Test
+    public void thereAndBackAgainTest() {
+        Assert.assertEquals(object, EncodeUtils.toObject(EncodeUtils.toBytes(object)));
+    }
+}
